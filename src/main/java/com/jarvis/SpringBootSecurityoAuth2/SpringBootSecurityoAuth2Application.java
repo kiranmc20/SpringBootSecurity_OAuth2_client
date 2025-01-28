5b36ffa4ece4e77034563e25c445870b6509c2de
Kiran <kiranmc20@gmail.com>
2025-01-28 20:09:56 +0530

    spring security OAuth2 client changes

---
+++ src/main/java/com/jarvis/SpringBootSecurityoAuth2/SpringBootSecurityoAuth2Application.java
@@ -0,0 +1,13 @@
+package com.jarvis.SpringBootSecurityoAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityoAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityoAuth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jarvis/SpringBootSecurityoAuth2/SpringBootSecurityoAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarvis.SpringBootSecurityoAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityoAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
